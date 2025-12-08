7cff4ddeadaf4beb6cd4270b59cf0625f6cc050a
Wouter Biront <wouterbiront@gmail.com>
2025-12-08 19:16:31 +0100

    Solved day 5 part one

--- src/main/java/day5/Main.java
+++ src/main/java/day5/Main.java
@@ -9,13 +9,34 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         List<String> input = readFile("src/main/resources/day5.txt");
+        List<String> idList;
+        List<String> ranges;
+
+        int freshIngredientAmount = 0;
+
+        ranges = input.subList(0, input.indexOf(""));
+        idList = input.subList(input.indexOf("") + 1, input.size());
+
+        for (String id : idList) {
+            long numericId = Long.parseLong(id);
+
+            for (String range : ranges) {
+                long first = Long.parseLong(range.split("-")[0]);
+                long last = Long.parseLong(range.split("-")[1]);
+
+                if (numericId >= first && numericId <= last) {
+                    freshIngredientAmount++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(freshIngredientAmount);
     }
 
     public static List<String> readFile(String fileName) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            List<String> lines = br.lines().toList();
-
-            return lines;
+            return br.lines().toList();
         }
     }
 }
