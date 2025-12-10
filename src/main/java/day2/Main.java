1bff215d42c3cdee1308cdf1db5aab4dda9e1201
Wouter Biront <wouterbiront@gmail.com>
2025-12-10 21:22:59 +0100

    Solved day 2 part twoe

--- src/main/java/day2/Main.java
+++ src/main/java/day2/Main.java
@@ -1,29 +1,21 @@
 package day2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        File myObj = new File("src/main/resources/day2.txt");
-        String data = "";
-        String[] idRanges;
-
+    public static void main(String[] args) throws IOException {
         String first;
         String last;
 
-        long sumInvalids = 0;
+        long sumInvalidsPartOne = 0;
+        long sumInvalidsPartTwo;
 
-        try (Scanner myReader = new Scanner(myObj)) {
-            while (myReader.hasNextLine()) {
-                data = myReader.nextLine();
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-        }
-
-        idRanges = data.split(",");
+        String[] idRanges = readAndProcessInput("src/main/resources/day2.txt");
+        List<Long> invalidIds = new ArrayList<>();
 
         for (String idRange : idRanges) {
             first = idRange.split("-")[0];
@@ -32,14 +24,46 @@ public class Main {
             for (long i = Long.parseLong(first); i <= Long.parseLong(last); i++) {
                 String current = String.valueOf(i);
 
-                if (current.substring(0, current.length() / 2).equals(current.substring(current.length() / 2))) {
-                    System.out.println("Invalid id: " + current);
-                    sumInvalids += Long.parseLong(current);
-                }
+                sumInvalidsPartOne += checkForInvalidPartOne(current);
+
+                invalidIds.add(checkForInvalidPartTwo(current));
             }
         }
 
-        System.out.println("Sum of invalid id's: " + sumInvalids);
+        System.out.println("PART 1: Sum of invalid id's: " + sumInvalidsPartOne);
+        sumInvalidsPartTwo = invalidIds.stream().distinct().mapToLong(Long::longValue).sum();
+        System.out.println("PART 2: Sum of invalid id's: " + sumInvalidsPartTwo);
+    }
+
+    public static String[] readAndProcessInput(String fileName) throws IOException {
+        try (BufferedReader bf = new BufferedReader(new FileReader(fileName))) {
+            return bf.readLine().split(",");
+        }
+    }
+
+    public static long checkForInvalidPartOne(String current) {
+        if (current.substring(0, current.length() / 2).equals(current.substring(current.length() / 2))) {
+            return Long.parseLong(current);
+        } else {
+            return 0;
+        }
+    }
+
+    public static long checkForInvalidPartTwo(String current) {
+        List<Integer> dividers = new ArrayList<>();
+        for (int i = 2; i <= current.length(); i++) {
+            if (current.length() % i == 0) {
+                dividers.add(i);
+            }
+        }
+
+        for (int divider : dividers) {
+            if (current.substring(0, current.length() / divider).repeat(divider).equals(current)) {
+                return Long.parseLong(current);
+            }
+        }
+
+        return 0L;
     }
 }
 
