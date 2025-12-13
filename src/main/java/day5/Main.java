2a99e91d9f5a3da3d4b1841c7348a26b31d28382
Wouter Biront <wouterbiront@gmail.com>
2025-12-13 18:01:59 +0100

    Trying to solve day5 part two

--- src/main/java/day5/Main.java
+++ src/main/java/day5/Main.java
@@ -3,6 +3,8 @@ package day5;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -17,6 +19,7 @@ public class Main {
         ranges = input.subList(0, input.indexOf(""));
         idList = input.subList(input.indexOf("") + 1, input.size());
 
+        // Part one: check whether an id from the list is present in any of the ranges.
         for (String id : idList) {
             long numericId = Long.parseLong(id);
 
@@ -31,7 +34,42 @@ public class Main {
             }
         }
 
-        System.out.println(freshIngredientAmount);
+        System.out.println("Part one: " + freshIngredientAmount);
+        System.out.println("#####");
+
+        // Part two: find out how many id's are fresh (all ranges).
+        List<Range> sortedRanges = ranges.stream()
+                .map(r -> {
+                    String[] parts = r.split("-");
+                    return new Range(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
+                })
+                .sorted(Comparator.comparingLong(Range::start))
+                .toList();
+
+        List<Range> mergedRanges = new ArrayList<>();
+        Range current = sortedRanges.get(0);
+
+        for (int i = 1; i < sortedRanges.size(); i++) {
+            Range next = sortedRanges.get(i);
+
+            if (current.end() >= next.start()) {
+                current = new Range(current.start(), next.end());
+            } else {
+                mergedRanges.add(current);
+                current = next;
+            }
+        }
+
+        mergedRanges.add(current);
+
+        long totalAmountFreshIngredients = 0;
+
+        for (Range range : mergedRanges) {
+            long sum = (range.end() - range.start()) + 1;
+            totalAmountFreshIngredients += sum;
+        }
+
+        System.out.println("Part two: " +  totalAmountFreshIngredients);
     }
 
     public static List<String> readFile(String fileName) throws IOException {
---
+++ src/main/java/day5/Range.java
@@ -0,0 +1,4 @@
+package day5;
+
+public record Range(long start, long end) {
+}
